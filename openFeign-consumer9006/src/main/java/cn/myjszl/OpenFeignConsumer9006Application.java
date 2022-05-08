b9b9adedc6b116100e47fa565ebcc3b563227cff
zhangheng <123@qq.com>
2022-05-08 22:37:11 +0800

    add name

--- openFeign-consumer9006/src/main/java/cn/myjszl/OpenFeignConsumer9006Application.java
+++ openFeign-consumer9006/src/main/java/cn/myjszl/OpenFeignConsumer9006Application.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
+ * @author Administrator
  * @EnableDiscoveryClient ：开启服务注册和发现
  */
 @SpringBootApplication
